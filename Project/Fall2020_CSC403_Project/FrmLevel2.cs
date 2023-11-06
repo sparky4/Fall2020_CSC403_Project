1217c610d0abd41cebd02f0cec4b6fd3c30bdefe
Trey Thomas <treythomas1979@yahoo.com>
2023-11-06 14:50:05 -0600

    Worked on collision boundaries to enter next level

---
+++ Project/Fall2020_CSC403_Project/FrmLevel2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+public class FrmLevel2 : Form
+{
+    public FrmLevel2()
+    {
+        // Initialize your level here
+        // Initialize your level here
+        this.Width = 1153; // Set the window width
+        this.Height = 600; // Set the window height
+        this.Text = "Level 2"; // Set the window title
+    }
+}
